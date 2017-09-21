850e70d0429dc90ff1469d000d62a5848222b3aa
sheng <289420843@qq.com>
2017-09-21 11:13:34 +0800

    Update Scroller.js

--- es/Scroller.js
+++ es/Scroller.js
@@ -891,10 +891,6 @@ var members = {
 
     var self = this;
 
-    if(self.options.onTouchEnd){
-      self.options.onTouchEnd();
-    }
-    
     // Ignore event when tracking is not enabled (no touchstart event on element)
     // This is required as this listener ('touchmove') sits on the document and not on the element itself.
     if (!self.__isTracking) {
@@ -995,6 +991,10 @@ var members = {
 
     // Fully cleanup list
     self.__positions.length = 0;
+    
+    if(self.options.onTouchEnd){
+      self.options.onTouchEnd(self);
+    }
   },
 
   /*
